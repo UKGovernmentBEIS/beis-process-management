2dbcb3ec863d1f7accee24b30626295962106425
venkata mutyala <venkatamutyala@venkatas-MacBook-Pro.local>
2017-07-13 11:25:36 +0100

    adding Activiti SQLs for postgress

---
+++ activiti-db-create/create-activiti-tables.sql
@@ -0,0 +1,23 @@
+-- \i activiti.db2.create.engine.sql
+-- \i activiti.db2.create.history.sql
+-- \i activiti.db2.create.identity.sql
+-- \i activiti.h2.create.engine.sql
+-- \i activiti.h2.create.history.sql
+-- \i activiti.h2.create.identity.sql
+-- \i activiti.hsql.create.engine.sql
+-- \i activiti.hsql.create.history.sql
+-- \i activiti.hsql.create.identity.sql
+-- \i activiti.mssql.create.engine.sql
+-- \i activiti.mssql.create.history.sql
+-- \i activiti.mssql.create.identity.sql
+-- \i activiti.mysql.create.engine.sql
+-- \i activiti.mysql.create.history.sql
+-- \i activiti.mysql.create.identity.sql
+-- \i activiti.mysql55.create.engine.sql
+-- \i activiti.mysql55.create.history.sql
+-- \i activiti.oracle.create.engine.sql
+-- \i activiti.oracle.create.history.sql
+-- \i activiti.oracle.create.identity.sql
+\i activiti.postgres.create.engine.sql
+\i activiti.postgres.create.history.sql
+\i activiti.postgres.create.identity.sql
